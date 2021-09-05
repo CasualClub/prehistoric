286c3b2ae553b7bbf898406e3d1dda5fb0839d6f
ColdWindBlows <liwindblows@gmail.com>
2021-09-05 12:38:43 +0800

    build project

---
+++ src/main/java/com/casualclub/prehistoric/PrehistoricApplication.java
@@ -0,0 +1,13 @@
+package com.casualclub.prehistoric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrehistoricApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrehistoricApplication.class, args);
+    }
+
+}
